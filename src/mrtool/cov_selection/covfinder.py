50df5ef00cadf7d538ca5cc870eaf791e9d63cef
zhengp0 <zhengp@uw.edu>
2020-05-25 20:38:38 -0700

    change beta_gprior std updating scheme in covfinder

--- src/mrtool/cov_selection/covfinder.py
+++ src/mrtool/cov_selection/covfinder.py
@@ -10,8 +10,8 @@ from mrtool import MRData, LinearCovModel, MRBRT
 class CovFinder:
     """Class in charge of the covariate selection.
     """
-    loose_beta_gprior_std = np.inf
-    dummy_gamma_uprior = np.array([1000.0, 1000.0])
+    loose_beta_gprior_std = 100.0
+    dummy_gamma_uprior = np.array([100.0, 100.0])
 
     def __init__(self,
                  data: MRData,
@@ -77,10 +77,16 @@ class CovFinder:
 
         if len(additional_covs) > 0:
             candidate_covs = self.selected_covs + additional_covs
+            candidate_cov_dict = {
+                cov: i for i, cov in enumerate(candidate_covs)
+            }
             gaussian_model = self.create_model(candidate_covs,
                                                prior_type='Gaussian')
             gaussian_model.fit_model(x0=np.zeros(gaussian_model.num_x_vars + gaussian_model.num_z_vars))
-            beta_soln_samples, _ = gaussian_model.sample_soln(sample_size=self.num_samples, sim_re=False)
+            beta_soln_samples, _ = gaussian_model.sample_soln(sample_size=self.num_samples,
+                                                              sim_prior=False,
+                                                              sim_re=False,
+                                                              print_level=5)
             beta_soln_mean = gaussian_model.beta_soln
             beta_soln_std = np.std(beta_soln_samples, axis=0)
             beta_soln_sig = self.is_significance(beta_soln_samples, var_type='beta')
@@ -93,8 +99,8 @@ class CovFinder:
             ])
             # update beta_gprior
             self.beta_gprior.update({
-                cov: np.array([beta_soln_mean[i], beta_soln_std[i]])
-                for i, cov in enumerate(self.selected_covs)
+                cov: np.array([beta_soln_mean[candidate_cov_dict[cov]], self.loose_beta_gprior_std])
+                for cov in self.selected_covs
                 if cov not in self.beta_gprior
             })
             # update the stop
