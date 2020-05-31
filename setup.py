8f7d6953d430e17f44784125be68a493e968d36e
zhengp0 <zhengp@uw.edu>
2020-05-31 12:08:13 -0700

    advance to version 0.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='mrtool',
-      version='0.0.0',
+      version='0.0.1',
       description='Meta-regression tool',
       url='https://github.com/ihmeuw-msca/SFMA',
       author='Peng Zheng',
