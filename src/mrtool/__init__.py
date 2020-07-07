ea7662682b5f520f07769dc4b8f21a0087786c25
zhengp0 <zhengp@uw.edu>
2020-07-07 14:19:45 -0700

    not preloading the scorelator for reticulate compatible reason

--- src/mrtool/__init__.py
+++ src/mrtool/__init__.py
@@ -10,4 +10,3 @@ from .core.cov_model import CovModel, LinearCovModel, LogCovModel
 from .core.model import MRBRT, MRBeRT
 from .core import utils
 from .cov_selection.covfinder import CovFinder
-from .evidence_score.scorelator import Scorelator
