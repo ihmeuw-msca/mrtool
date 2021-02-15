75836119d47c2c659934a6714eb131d9629900b1
zhengp0 <zhengp@uw.edu>
2021-02-15 14:36:52 -0800

    move the adjust_orig_val function for use of xdata

--- src/mrtool/xwalk/model.py
+++ src/mrtool/xwalk/model.py
@@ -19,6 +19,7 @@ from . import utils
 class CovModel:
     """Covariate model.
     """
+
     def __init__(self, cov_name,
                  spline=None,
                  spline_monotonicity=None,
@@ -61,8 +62,8 @@ class CovModel:
         self.spline_convexity = spline_convexity
         self.use_spline = spline is not None
         self.use_constraints = self.use_spline and (
-                self.spline_monotonicity is not None or
-                self.spline_convexity is not None
+            self.spline_monotonicity is not None or
+            self.spline_convexity is not None
         )
         self.soln_name = cov_name if soln_name is None else soln_name
         self.prior_beta_uniform = {} if prior_beta_uniform is None else prior_beta_uniform
@@ -73,20 +74,20 @@ class CovModel:
         else:
             self.num_vars = 1
 
-    def create_design_mat(self, cwdata):
+    def create_design_mat(self, xdata):
         """Create design matrix.
 
         Args:
-            cwdata (crosswalk.CWData):
+            xdata (crosswalk.xdata):
                 Data structure has all the information.
 
         Returns:
             numpy.ndarray:
                 Return the design matrix from linear cov or spline.
         """
-        assert self.cov_name in cwdata.covs.columns,\
+        assert self.cov_name in xdata.covs.columns,\
             "Unkown covariates, not appear in the data."
-        cov = cwdata.covs[self.cov_name].values
+        cov = xdata.covs[self.cov_name].values
         if self.use_spline:
             mat = self.spline.design_mat(cov)[:, 1:]
         else:
@@ -130,7 +131,8 @@ class CovModel:
 class CWModel:
     """Cross Walk model.
     """
-    def __init__(self, cwdata,
+
+    def __init__(self, xdata,
                  obs_type='diff_log',
                  cov_models=None,
                  gold_dorm=None,
@@ -140,7 +142,7 @@ class CWModel:
                  prior_gamma_gaussian=None):
         """Constructor of CWModel.
         Args:
-            cwdata (data.CWData):
+            xdata (data.xdata):
                 Data for cross walk.
             obs_type (str, optional):
                 Type of observation can only be chosen from `'diff_log'` and
@@ -158,14 +160,14 @@ class CWModel:
             prior_gamma_gaussian (Tuple[float, float], optional):
                 If not ``None``, use it as the gaussian prior of gamma.
         """
-        self.cwdata = cwdata
+        self.xdata = xdata
         self.obs_type = obs_type
         self.cov_models = utils.default_input(cov_models,
                                               [CovModel('intercept')])
-        self.gold_dorm = utils.default_input(gold_dorm, cwdata.max_ref_dorm)
+        self.gold_dorm = utils.default_input(gold_dorm, xdata.max_ref_dorm)
         self.order_prior = order_prior
         self.use_random_intercept = use_random_intercept
-        if self.cwdata.num_studies == 0 and self.use_random_intercept:
+        if self.xdata.num_studies == 0 and self.use_random_intercept:
             warnings.warn("Must have study_id to use random intercept."
                           " Reset use_random_intercept to False.")
             self.use_random_intercept = False
@@ -191,14 +193,14 @@ class CWModel:
         self.obs_inv_fun = obs_inv_fun
 
         # variable names
-        self.vars = [dorm for dorm in self.cwdata.unique_dorms]
+        self.vars = [dorm for dorm in self.xdata.unique_dorms]
 
         # dimensions
         self.num_vars_per_dorm = sum([model.num_vars for model in self.cov_models])
-        self.num_vars = self.num_vars_per_dorm*self.cwdata.num_dorms
+        self.num_vars = self.num_vars_per_dorm*self.xdata.num_dorms
 
         # indices for easy access the variables
-        var_sizes = np.array([self.num_vars_per_dorm]*self.cwdata.num_dorms)
+        var_sizes = np.array([self.num_vars_per_dorm]*self.xdata.num_dorms)
         var_idx = utils.sizes_to_indices(var_sizes)
         self.var_idx = {
             var: var_idx[i]
@@ -215,7 +217,8 @@ class CWModel:
         self.constraint_mat = self.create_constraint_mat()
 
         # gamma bounds
-        self.prior_gamma_uniform = np.array([0.0, np.inf]) if prior_gamma_uniform is None else np.array(prior_gamma_uniform)
+        self.prior_gamma_uniform = np.array(
+            [0.0, np.inf]) if prior_gamma_uniform is None else np.array(prior_gamma_uniform)
         if not self.use_random_intercept:
             self.prior_gamma_uniform = np.zeros(2)
         if self.prior_gamma_uniform[0] < 0.0:
@@ -223,7 +226,8 @@ class CWModel:
             self.prior_gamma_uniform[0] = 0.0
 
         # gamma Gaussian prior
-        self.prior_gamma_gaussian = np.array([0.0, np.inf]) if prior_gamma_gaussian is None else np.array(prior_gamma_gaussian)
+        self.prior_gamma_gaussian = np.array(
+            [0.0, np.inf]) if prior_gamma_gaussian is None else np.array(prior_gamma_gaussian)
         if not self.use_random_intercept:
             self.prior_gamma_gaussian = np.array([0.0, np.inf])
 
@@ -253,13 +257,13 @@ class CWModel:
     def check(self):
         """Check input type, dimension and values.
         """
-        assert isinstance(self.cwdata, data.CWData)
+        assert isinstance(self.xdata, data.xdata)
         assert self.obs_type in ['diff_log', 'diff_logit'], \
             "Unsupport observation type"
         assert isinstance(self.cov_models, list)
         assert all([isinstance(model, CovModel) for model in self.cov_models])
 
-        assert self.gold_dorm in self.cwdata.unique_dorms
+        assert self.gold_dorm in self.xdata.unique_dorms
 
         assert self.order_prior is None or isinstance(self.order_prior, list)
 
@@ -275,36 +279,36 @@ class CWModel:
         """Check the rank of the design matrix.
         """
         rank = np.linalg.matrix_rank(self.design_mat)
-        num_unknowns = self.num_vars_per_dorm*(self.cwdata.num_dorms - 1)
+        num_unknowns = self.num_vars_per_dorm*(self.xdata.num_dorms - 1)
         if rank < num_unknowns:
             raise ValueError(f"Not enough information in the data to recover parameters."
                              f"Number of effective data points is {rank} and number of unknowns is {num_unknowns}."
                              f"Please include more effective data or reduce the number of covariates.")
 
-    def create_relation_mat(self, cwdata=None):
+    def create_relation_mat(self, xdata=None):
         """Creating relation matrix.
 
         Args:
-            cwdata (data.CWData | None, optional):
-                Optional data set, if None, use `self.cwdata`.
+            xdata (data.xdata | None, optional):
+                Optional data set, if None, use `self.xdata`.
 
         Returns:
             numpy.ndarray:
                 Returns relation matrix with 1 encode alternative definition
                 and -1 encode reference definition.
         """
-        cwdata = utils.default_input(cwdata,
-                                     default=self.cwdata)
-        assert isinstance(cwdata, data.CWData)
+        xdata = utils.default_input(xdata,
+                                    default=self.xdata)
+        assert isinstance(xdata, data.xdata)
 
-        relation_mat = np.zeros((cwdata.num_obs, cwdata.num_dorms))
-        for i, dorms in enumerate(cwdata.alt_dorms):
+        relation_mat = np.zeros((xdata.num_obs, xdata.num_dorms))
+        for i, dorms in enumerate(xdata.alt_dorms):
             for dorm in dorms:
-                relation_mat[i, cwdata.dorm_idx[dorm]] += 1.0
+                relation_mat[i, xdata.dorm_idx[dorm]] += 1.0
 
-        for i, dorms in enumerate(cwdata.ref_dorms):
+        for i, dorms in enumerate(xdata.ref_dorms):
             for dorm in dorms:
-                relation_mat[i, cwdata.dorm_idx[dorm]] -= 1.0
+                relation_mat[i, xdata.dorm_idx[dorm]] -= 1.0
 
         return relation_mat
 
@@ -312,40 +316,40 @@ class CWModel:
         """Check relation matrix, detect unused dorms.
         """
         col_scales = np.max(np.abs(self.relation_mat), axis=0)
-        unused_dorms = [self.cwdata.unique_dorms[i]
+        unused_dorms = [self.xdata.unique_dorms[i]
                         for i, scale in enumerate(col_scales) if scale == 0.0]
         if len(unused_dorms) != 0:
             raise ValueError(f"{unused_dorms} appears to be unused, most likely it is (they are) "
                              f"in both alt_dorms and ref_dorms at the same time for all its (their) "
                              f"appearance. Please remove {unused_dorms} from alt_dorms and ref_dorms.")
 
-    def create_cov_mat(self, cwdata=None):
+    def create_cov_mat(self, xdata=None):
         """Create covariates matrix for definitions/methods model.
 
         Args:
-            cwdata (data.CWData | None, optional):
-                Optional data set, if None, use `self.cwdata`.
+            xdata (data.xdata | None, optional):
+                Optional data set, if None, use `self.xdata`.
 
         Returns:
             numpy.ndarray:
                 Returns covarites matrix.
         """
-        cwdata = utils.default_input(cwdata,
-                                     default=self.cwdata)
-        assert isinstance(cwdata, data.CWData)
+        xdata = utils.default_input(xdata,
+                                    default=self.xdata)
+        assert isinstance(xdata, data.xdata)
 
-        return np.hstack([model.create_design_mat(cwdata)
+        return np.hstack([model.create_design_mat(xdata)
                           for model in self.cov_models])
 
     def create_design_mat(self,
-                          cwdata=None,
+                          xdata=None,
                           relation_mat=None,
                           cov_mat=None):
         """Create linear design matrix.
 
         Args:
-            cwdata (data.CWData | None, optional):
-                Optional data set, if None, use `self.cwdata`.
+            xdata (data.xdata | None, optional):
+                Optional data set, if None, use `self.xdata`.
             relation_mat (numpy.ndarray | None, optional):
                 Optional relation matrix, if None, use `self.relation_mat`
             cov_mat (numpy.ndarray | None, optional):
@@ -355,17 +359,17 @@ class CWModel:
             numpy.ndarray:
                 Returns linear design matrix.
         """
-        cwdata = utils.default_input(cwdata,
-                                     default=self.cwdata)
+        xdata = utils.default_input(xdata,
+                                    default=self.xdata)
         relation_mat = utils.default_input(relation_mat,
                                            default=self.relation_mat)
         cov_mat = utils.default_input(cov_mat,
-                                           default=self.cov_mat)
+                                      default=self.cov_mat)
 
         mat = (
             relation_mat.ravel()[:, None] *
-            np.repeat(cov_mat, cwdata.num_dorms, axis=0)
-        ).reshape(cwdata.num_obs, self.num_vars)
+            np.repeat(cov_mat, xdata.num_dorms, axis=0)
+        ).reshape(xdata.num_obs, self.num_vars)
 
         return mat
 
@@ -422,15 +426,15 @@ class CWModel:
                 and the less quality of trimming it will guarantee.
         """
         # dimensions for limetr
-        n = self.cwdata.study_sizes
+        n = self.xdata.study_sizes
         if n.size == 0:
-            n = np.full(self.cwdata.num_obs, 1)
+            n = np.full(self.xdata.num_obs, 1)
         k_beta = self.num_vars
         k_gamma = 1
-        y = self.cwdata.obs
-        s = self.cwdata.obs_se
+        y = self.xdata.obs
+        s = self.xdata.obs_se
         x = self.design_mat
-        z = np.ones((self.cwdata.num_obs, 1))
+        z = np.ones((self.xdata.num_obs, 1))
 
         uprior = np.hstack((self.prior_beta_uniform, self.prior_gamma_uniform[:, None]))
         gprior = np.hstack((self.prior_beta_gaussian, self.prior_gamma_gaussian[:, None]))
@@ -480,7 +484,7 @@ class CWModel:
             u = self.lt.estimateRE()
             self.random_vars = {
                 sid: u[i]
-                for i, sid in enumerate(self.cwdata.unique_study_id)
+                for i, sid in enumerate(self.xdata.unique_study_id)
             }
         else:
             self.random_vars = dict()
@@ -489,7 +493,7 @@ class CWModel:
         hessian = self.get_beta_hessian()
         unconstrained_id = np.hstack([
             np.arange(self.lt.k_beta)[self.var_idx[dorm]]
-            for dorm in self.cwdata.unique_dorms
+            for dorm in self.xdata.unique_dorms
             if dorm != self.gold_dorm
         ])
         self.beta_sd = np.zeros(self.lt.k_beta)
@@ -531,9 +535,9 @@ class CWModel:
             pd.DataFrame: Data frame that contains the result.
         """
         # column of dorms
-        dorms = np.repeat(self.cwdata.unique_dorms, self.num_vars_per_dorm)
+        dorms = np.repeat(self.xdata.unique_dorms, self.num_vars_per_dorm)
         cov_names = self.get_cov_names()
-        cov_names *= self.cwdata.num_dorms
+        cov_names *= self.xdata.num_dorms
 
         # create data frame
         df = pd.DataFrame({
@@ -544,9 +548,9 @@ class CWModel:
         })
         if self.use_random_intercept:
             gamma = np.hstack((self.lt.gamma, np.full(self.num_vars - 1, np.nan)))
-            re = np.hstack((self.lt.u, np.full((self.cwdata.num_studies, self.num_vars - 1), np.nan)))
+            re = np.hstack((self.lt.u, np.full((self.xdata.num_studies, self.num_vars - 1), np.nan)))
             df['gamma'] = gamma
-            for i, study_id in enumerate(self.cwdata.unique_study_id):
+            for i, study_id in enumerate(self.xdata.unique_study_id):
                 df[study_id] = re[i]
 
         return df
@@ -562,114 +566,3 @@ class CWModel:
             filename += '.csv'
         df = self.create_result_df()
         df.to_csv(folder + '/' + filename, index=False)
-
-    def adjust_orig_vals(self, df,
-                         orig_dorms,
-                         orig_vals_mean,
-                         orig_vals_se,
-                         study_id=None,
-                         data_id=None,
-                         ref_dorms=None):
-        """Adjust alternative values.
-
-        Args:
-            df (pd.DataFrame):
-                Data frame of the alternative values that need to be adjusted.
-            orig_dorms (str):
-                Name of the column in `df` that contains the alternative
-                definitions or methods.
-            orig_vals_mean (str):
-                Name of the column in `df` that contains the alternative values.
-            orig_vals_se (str):
-                Name of the column in `df` that contains the standard error of
-                alternative values.
-            study_id (str | None, optional):
-                If not `None`, predict with the random effects.
-            data_id (str | None, optional):
-                If `None` create data_id by the integer sequence.
-            ref_dorms (str, optional):
-                Name of the column with reference dorms, if is ``None``, use the
-                gold_dorm as the reference dorm. Default to ``None``.
-
-        Returns:
-            pandas.DataFrame:
-                The adjusted values and standard deviations.
-        """
-        df_copy = df.copy()
-        if ref_dorms is None:
-            ref_dorms = 'ref_dorms'
-            df_copy[ref_dorms] = np.array([self.gold_dorm]*df_copy.shape[0])
-        if 'intercept' not in df_copy.columns:
-            df_copy['intercept'] = np.ones(df_copy.shape[0])
-        new_cwdata = data.CWData(df_copy,
-                                 alt_dorms=orig_dorms,
-                                 ref_dorms=ref_dorms,
-                                 dorm_separator=self.cwdata.dorm_separator,
-                                 covs=list(self.cwdata.covs.columns),
-                                 data_id=data_id,
-                                 add_intercept=False)
-
-        # transfer data dorm structure to the new_cwdata
-        new_cwdata.copy_dorm_structure(self.cwdata)
-
-        # create new design matrix
-        new_relation_mat = self.create_relation_mat(cwdata=new_cwdata)
-        new_cov_mat = self.create_cov_mat(cwdata=new_cwdata)
-        new_design_mat = self.create_design_mat(cwdata=new_cwdata,
-                                                relation_mat=new_relation_mat,
-                                                cov_mat=new_cov_mat)
-
-        # calculate the random effects
-        if study_id is not None:
-            random_effects = np.array([
-                self.random_vars[sid][0]
-                if sid in self.random_vars else 0.0
-                for sid in df[study_id]
-            ])
-        else:
-            random_effects = np.zeros(df.shape[0])
-        random_effects[df[orig_dorms].values == self.gold_dorm] = 0.0
-
-        # compute the corresponding gold_dorm value
-        if self.obs_type == 'diff_log':
-            transformed_orig_vals_mean,\
-            transformed_orig_vals_se = utils.linear_to_log(
-                df[orig_vals_mean].values,
-                df[orig_vals_se].values
-            )
-        else:
-            transformed_orig_vals_mean, \
-            transformed_orig_vals_se = utils.linear_to_logit(
-                df[orig_vals_mean].values,
-                df[orig_vals_se].values
-            )
-
-        pred_diff_mean = new_design_mat.dot(self.beta)
-        pred_diff_sd = np.sqrt(np.array([
-            (new_design_mat[i]**2).dot(self.beta_sd**2)
-            if dorm != self.gold_dorm else 0.0
-            for i, dorm in enumerate(df[orig_dorms])
-        ]))
-
-        transformed_ref_vals_mean = transformed_orig_vals_mean - \
-            pred_diff_mean - random_effects
-        transformed_ref_vals_sd = np.sqrt(transformed_orig_vals_se**2 +
-                                          pred_diff_sd**2 + self.gamma[0]**2)
-        if self.obs_type == 'diff_log':
-            ref_vals_mean,\
-            ref_vals_sd = utils.log_to_linear(transformed_ref_vals_mean,
-                                              transformed_ref_vals_sd)
-        else:
-            ref_vals_mean,\
-            ref_vals_sd = utils.logit_to_linear(transformed_ref_vals_mean,
-                                                transformed_ref_vals_sd)
-
-        pred_df = pd.DataFrame({
-            'ref_vals_mean': ref_vals_mean,
-            'ref_vals_sd': ref_vals_sd,
-            'pred_diff_mean': pred_diff_mean,
-            'pred_diff_sd': pred_diff_sd,
-            'data_id': new_cwdata.data_id
-        })
-
-        return pred_df
