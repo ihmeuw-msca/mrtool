b52739567103b96a2a43a28b67b5568043a34f5e
zhengp0 <zhengp@uw.edu>
2021-03-30 12:59:21 -0700

    adjust and test netmrdata

--- src/mrtool/netmr/data.py
+++ src/mrtool/netmr/data.py
@@ -5,8 +5,8 @@
 
     `data` module of the `crosswalk` package.
 """
-from typing import Dict
-from collections import Counter
+from typing import Dict, Iterable
+from collections import Counter, defaultdict
 from dataclasses import dataclass
 import numpy as np
 from numpy import ndarray
@@ -61,7 +61,6 @@ class AltDormColumn(DormColumn):
 class NetMRData(MRData):
     """Data for network meta analysis model.
     """
-    # need memory of the dorm structure
 
     def __init__(self,
                  ref_dorm: str = RefDormColumn.name,
@@ -74,32 +73,32 @@ class NetMRData(MRData):
         self.add_column(self.ref_dorm)
         self.add_column(self.alt_dorm)
         self.unique_dorms = []
-        self.dorm_counts = {}
 
     @MRData.df.setter
     def df(self, df: DataFrame):
         super(NetMRData, type(self)).df.fset(self, df)
-
         if not self.unique_dorms:
-            self.unique_dorms = list(np.unique(np.hstack([
-                self.ref_dorm.unique_values,
-                self.alt_dorm.unique_values
-            ])))
+            self.set_unique_dorms(np.hstack([self.ref_dorm.unique_values,
+                                             self.alt_dorm.unique_values]))
 
-        if not self.dorm_counts:
-            counter = Counter()
-            counter.update(self.ref_dorm.value_counts)
-            counter.update(self.alt_dorm.value_counts)
-            self.dorm_counts = dict(counter)
+    @property
+    def dorm_counts(self) -> Dict:
+        counter = Counter()
+        counter.update(self.ref_dorm.value_counts)
+        counter.update(self.alt_dorm.value_counts)
+        return defaultdict(int, counter)
+
+    def set_unique_dorms(self, dorms: Iterable):
+        self.unique_dorms = list(np.unique(dorms))
 
     def get_relation_mat(self) -> ndarray:
         self._assert_not_empty()
-        mat = np.zeros(self.shape[0], len(self.unique_dorms))
+        mat = np.zeros((self.shape[0], len(self.unique_dorms)))
 
         for i in range(self.shape[0]):
-            for dorm in self.alt_dorm.values:
+            for dorm in self.alt_dorm.values[i]:
                 mat[i, self.unique_dorms.index(dorm)] += 1.0
-            for dorm in self.ref_dorm.values:
+            for dorm in self.ref_dorm.values[i]:
                 mat[i, self.unique_dorms.index(dorm)] -= 1.0
 
         return mat
