129dc04cbce99b81552a0522701ec3e94275304d
zhengp0 <zhengp@uw.edu>
2021-04-06 14:32:20 -0700

    add all important imports into the __init__

--- src/mrtool/__init__.py
+++ src/mrtool/__init__.py
@@ -5,8 +5,7 @@
 
     `mrtool` package.
 """
+from regmod.utils import SplineSpecs
 from .core.data import MRData
 from .core.cov_model import CovModel, LinearCovModel, LogCovModel
 from .core.model import MRBRT
-from .core import utils
-# from .cov_selection.covfinder import CovFinder
